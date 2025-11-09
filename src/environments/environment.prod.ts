dfe2ba75ffdf2668e80cc7a0635d0265ddf578ef
jsepulveda-datascientist <jorge.sepulveda.munoz@gmail.com>
2025-11-09 13:31:40 -0300

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: true,
-  apiBase: 'https://api.karaqr.com',
-  defaultTenant: 'default'
+  production: false,
+  apiBase: 'https://sphllmtaepeeiozqljsb.supabase.co',
+  defaultTenant: 'demo-venue',
+  supabaseUrl: 'https://sphllmtaepeeiozqljsb.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwaGxsbXRhZXBlZWlvenFsanNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1ODYxNjMsImV4cCI6MjA3ODE2MjE2M30.ekXTrCSUw-SNZL1_8KJLnYmdgS_pnLmBeTK1Bwr1lss'
 };
